d743fd71db1d165a80516641171007756d8c9971
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-14 20:03:35 -0300

    Criando os components do projeto

--- secret-word-app/src/App.jsx
+++ secret-word-app/src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+// CSS
+import './App.css';
 
-import { StartScreen } from './components'
+// REACT
+import { useState, useEffect, useCallback } from 'react';
+
+// DATA
+import { wordsList } from './data/word'
+
+// COMPONENTS
+import { StartScreen, Game, GameOver } from './components'
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words);
 
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   )
 }
--- secret-word-app/src/components/index.js
+++ secret-word-app/src/components/index.js
@@ -1,1 +1,3 @@
 export { default as StartScreen } from './StartScreen.jsx';
+export { default as Game } from './Game.jsx';
+export { default as GameOver } from './GameOver.jsx';
