fcf153e111caf9e8e977f4cc4d7ac2691a36e4d8
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-13 21:03:08 -0300

    Finalizando tela principal

---
+++ secret-word-app/src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <section className='start'>
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar.</p>
+      <button>Começar o jogo.</button>
+    </section>
+  )
+}
+
+export default StartScreen
